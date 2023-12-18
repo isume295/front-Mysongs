953477ac5d93260e15171e77a11884944a082d5d
Sumeya <sumeyaibrahim72@gmail.com>
2023-12-18 15:44:24 +0300

    Configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+    return <div className="App bg-rose-400">hi</div>;
 }
 
 export default App;
