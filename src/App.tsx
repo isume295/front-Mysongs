72a64625034496f8e4238f86ca8fd7ecaffd52f1
Sumeya <sumeyaibrahim72@gmail.com>
2023-12-19 22:07:53 +0300

    Update audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import './App.css';
-import { Hero } from './components/Hero';
 import { Navbar } from './components/Navbar';
-import audioSource from './assets/audio/Sappheiros - Embrace.mp3';
-import { useSelector } from 'react-redux';
-import { Statistics } from './components/Statistics';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import SongsPage from './pages/SongsPage';
 import AddSongForm from './components/AddSongForm';
 import Details from './components/Details';
+import { Audio } from './components/Audio';
 
 function App() {
-    const audioRef = useRef<HTMLAudioElement>(null);
-
-    const handlePlay = () => {
-        if (audioRef.current) {
-            audioRef.current.play();
-        }
-    };
-
     return (
         <div className="App bg-gray-50 relative">
             <Navbar />
@@ -29,15 +18,7 @@ function App() {
                 <Route path="/new-song" element={<AddSongForm />} />
                 <Route path="/details/:id" element={<Details />} />
             </Routes>
-            <audio ref={audioRef}>
-                <source src={audioSource} type="audio/mp3" />
-                Your browser does not support the audio element.
-            </audio>
-            <div className="fixed right-5 z-10 bottom-5">
-                <button className="w-16 h-16 rounded-full bg-blue-500 text-white flex items-center justify-center" onClick={handlePlay}>
-                    Play Audio
-                </button>
-            </div>
+            <Audio />
         </div>
     );
 }
